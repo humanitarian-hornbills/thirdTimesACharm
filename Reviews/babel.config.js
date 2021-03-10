dd72c5253163f559995914a7427c6bb4f74eb346
gusfel <agustinjfeliciano@gmail.com>
2021-03-09 20:02:15 -0800

    trying to get aws to work

--- Reviews/babel.config.js
+++ Reviews/babel.config.js
@@ -1,13 +1,3 @@
 module.exports = {
   presets: ['@babel/preset-env', '@babel/preset-react'],
-  plugins: [
-    'babel-plugin-styled-components',
-    {
-      'ssr': false
-    },
-    {
-      "displayName": false
-    },
-    
-  ]
 };

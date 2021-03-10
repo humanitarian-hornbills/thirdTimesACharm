43000a4bbb1f58c24c75ccf96e7feeedca1ffbc7
jincheng411 <jincheng411@gmail.com>
2021-03-10 12:38:37 -0500

    hide scroll btn when page load

--- RelatedProducts/src/components/ScrollBtn.jsx
+++ RelatedProducts/src/components/ScrollBtn.jsx
@@ -32,9 +32,9 @@ const ScrollBtn = ({ direction, list }) => {
     }
   };
   let scrollBtn = '';
-  if (direction === 'left') {
+  if (direction === 'left' && !inBoundary.isInboundaryOp(list, 'left')) {
     scrollBtn = <div className="scroll-btn-left btn-disable" onClick={handleScroll} onKeyDown={handleScroll} role="button" tabIndex={0}><span className="scroll-btn-arrow">&lt;</span></div>;
-  } else if (direction === 'right') {
+  } else if (direction === 'right' && !inBoundary.isInboundaryOp(list, 'right')) {
     scrollBtn = <div className="scroll-btn-right btn-disable" onClick={handleScroll} onKeyDown={handleScroll} role="button" tabIndex={0}><span className="scroll-btn-arrow">&gt;</span></div>;
   }
   return (

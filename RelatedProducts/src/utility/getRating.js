af06457d87dc98b753b416527d369e0e72223dfa
jincheng411 <jincheng411@gmail.com>
2021-03-10 18:32:41 -0500

    display rating properly

--- RelatedProducts/src/utility/getRating.js
+++ RelatedProducts/src/utility/getRating.js
@@ -3,12 +3,12 @@ import axios from 'axios';
 const getRating = (productId, callback) => {
   axios.get(`./reviews/${productId}`)
     .then((res) => {
-      const { results } = res.data;
-      const total = results.reduce((sum, review) => (
-        sum + review.rating
-      ), 0);
-      const average = total / results.length;
-      callback(average);
+      // const { results } = res.data;
+      // const total = results.reduce((sum, review) => (
+      //   sum + review.rating
+      // ), 0);
+      // const average = total / results.length;
+      callback(Number(res.data));
     });
 };
 export default getRating;
